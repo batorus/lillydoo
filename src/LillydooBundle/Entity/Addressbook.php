d41a5e455a7f00bad3bd6a62c73cb0e391946659
batorus <batoru@yahoo.com>
2019-05-29 17:04:27 +0300

    add addressbook entity

---
+++ src/LillydooBundle/Entity/Addressbook.php
@@ -0,0 +1,313 @@
+<?php
+
+namespace LillydooBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Addressbook
+ *
+ * @ORM\Table(name="addressbook")
+ * @ORM\Entity(repositoryClass="LillydooBundle\Repository\AddressbookRepository")
+ */
+class Addressbook
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="firstname", type="string", length=50, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="lastname", type="string", length=50, nullable=true)
+     */
+    private $lastname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street", type="string", length=50)
+     */
+    private $street;
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="number", type="integer", nullable=true)
+     */
+    private $number;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="country", type="string", length=50, nullable=true)
+     */
+    private $country;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="phonenumber", type="string", length=50, nullable=true)
+     */
+    private $phonenumber;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthday", type="datetime")
+     */
+    private $birthday;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="email", type="string", length=50, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="picture", type="string", length=50, nullable=true)
+     */
+    private $picture;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstname.
+     *
+     * @param string|null $firstname
+     *
+     * @return Addressbook
+     */
+    public function setFirstname($firstname = null)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname.
+     *
+     * @return string|null
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set lastname.
+     *
+     * @param string|null $lastname
+     *
+     * @return Addressbook
+     */
+    public function setLastname($lastname = null)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname.
+     *
+     * @return string|null
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set street.
+     *
+     * @param string $street
+     *
+     * @return Addressbook
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get street.
+     *
+     * @return string
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set number.
+     *
+     * @param int|null $number
+     *
+     * @return Addressbook
+     */
+    public function setNumber($number = null)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number.
+     *
+     * @return int|null
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * Set country.
+     *
+     * @param string|null $country
+     *
+     * @return Addressbook
+     */
+    public function setCountry($country = null)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country.
+     *
+     * @return string|null
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set phonenumber.
+     *
+     * @param string|null $phonenumber
+     *
+     * @return Addressbook
+     */
+    public function setPhonenumber($phonenumber = null)
+    {
+        $this->phonenumber = $phonenumber;
+
+        return $this;
+    }
+
+    /**
+     * Get phonenumber.
+     *
+     * @return string|null
+     */
+    public function getPhonenumber()
+    {
+        return $this->phonenumber;
+    }
+
+    /**
+     * Set birthday.
+     *
+     * @param \DateTime $birthday
+     *
+     * @return Addressbook
+     */
+    public function setBirthday($birthday)
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+
+    /**
+     * Get birthday.
+     *
+     * @return \DateTime
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * Set email.
+     *
+     * @param string|null $email
+     *
+     * @return Addressbook
+     */
+    public function setEmail($email = null)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email.
+     *
+     * @return string|null
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set picture.
+     *
+     * @param string|null $picture
+     *
+     * @return Addressbook
+     */
+    public function setPicture($picture = null)
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * Get picture.
+     *
+     * @return string|null
+     */
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+}
