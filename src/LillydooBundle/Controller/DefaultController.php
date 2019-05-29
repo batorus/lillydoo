56b6ec768dfde16e9a298fb462d1cdeafbff978c
batorus <batoru@yahoo.com>
2019-05-29 15:34:14 +0300

    start integration with bootstrap 4.3

--- src/LillydooBundle/Controller/DefaultController.php
+++ src/LillydooBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('LillydooBundle:Default:index.html.twig');
+        return $this->render('@Lillydoo/Default/index.html.twig');
     }
 }
